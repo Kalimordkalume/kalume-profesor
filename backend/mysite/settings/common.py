c7746448a290eec8abfcf9336c595b48a41cff49
Kalume <abrahammoracejas@gmail.com>
2024-03-16 11:17:14 +0100

    asdasd

--- backend/mysite/settings/common.py
+++ backend/mysite/settings/common.py
@@ -103,11 +103,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    BASE_DIR / "static/css",
-]
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -147,3 +144,8 @@ LOGGING = {
         }
     },
 }
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_files/"),
+]
