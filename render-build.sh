b732c3e62ba139c52f7dc92ed13c87685d613466
gilberto <levin@readytheory.com>
2024-03-17 15:56:31 -0500

    cd to backend then requirements.txt

--- render-build.sh
+++ render-build.sh
@@ -3,8 +3,9 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r backend.requirements.txt
+
 cd ./backend
+pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
