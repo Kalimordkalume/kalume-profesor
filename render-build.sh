b7b11d1a283fc3186aaa2f22a6a956e2d7937c53
Kalume <abrahammoracejas@gmail.com>
2024-03-18 15:39:06 +0100

    sda

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 
 cd ./backend && pipx install poetry && poetry install && poetry env use python3.12.2
-
+echo 'I executed properly by my own Kalume'
 
 # Convert static asset files
 python manage.py collectstatic --no-input
