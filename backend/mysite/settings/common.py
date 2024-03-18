30561811c583f40179541489e136bb5c00fbc5ea
Kalume <abrahammoracejas@gmail.com>
2024-03-18 23:15:24 +0100

    Deleted reference in settings for django debug

--- backend/mysite/settings/common.py
+++ backend/mysite/settings/common.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "rest_framework",
     "pages",
     "calculator",
