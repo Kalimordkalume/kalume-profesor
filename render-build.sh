e2aadb10c9eb3ec2563f1d3375285f67df5bc3fc
Kalume <abrahammoracejas@gmail.com>
2024-03-18 15:27:52 +0100

    render-build.sh modifed with custom commands for poetry

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 
 cd ./backend
-pip install -r requirements.txt
+pip install poetry
+poetry install
 # Convert static asset files
 python manage.py collectstatic --no-input
 
